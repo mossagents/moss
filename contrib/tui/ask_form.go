e44b08beb9fd9dc95c3b03c64047f9de4ce1ad31
Qiulin Li <qlin.dev@outlook.com>
2026-04-21 23:43:29 +0800

    tui: add separator rule above inline approval/confirm dialogs

--- contrib/tui/ask_form.go
+++ contrib/tui/ask_form.go
@@ -1122,6 +1122,11 @@ func (m chatModel) renderInlineApprovalAsk(width int) string {
 
 	var sb strings.Builder
 
+	// 上方与流式内容的分隔横线
+	ruleWidth := max(1, width-1)
+	sb.WriteString(shellRuleStyle.Render(strings.Repeat("─", ruleWidth)))
+	sb.WriteString("\n")
+
 	// セクションヘッダー
 	sb.WriteString(dialogAccentStyle.Render("□  " + display.Title))
 	sb.WriteString("\n\n")
@@ -1185,6 +1190,11 @@ func (m chatModel) renderInlineSimpleConfirmAsk(width int) string {
 	yesSelected := field.boolValue
 
 	var sb strings.Builder
+
+	// 上方与流式内容的分隔横线
+	ruleWidth := max(1, width-1)
+	sb.WriteString(shellRuleStyle.Render(strings.Repeat("─", ruleWidth)))
+	sb.WriteString("\n")
 
 	// タイトルバッジ
 	title := confirmDialogTitle(m.pendAsk.request)
